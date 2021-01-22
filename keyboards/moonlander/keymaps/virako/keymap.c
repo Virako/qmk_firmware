18e3bec0374fa72a294792d543070e8c7aea9465
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-01-22 10:26:11 +0100

    Functions keys with tap f, vim copy and paste, spanish easier

--- keyboards/moonlander/keymaps/virako/keymap.c
+++ keyboards/moonlander/keymaps/virako/keymap.c
@@ -46,10 +46,10 @@ enum custom_keycodes {
   ST_MACRO_Q,
   ST_MACRO_E,
   ST_MACRO_R,
-  ST_MACRO_N,
-  ST_MACRO_U,
-  ST_MACRO_VQ,
-  ST_MACRO_VW,
+  VIM_QUIT,
+  VIM_SAVE,
+  VIM_COPY,
+  VIM_PASTE,
   ES_LSPO,
   ES_RSPC,
 };
@@ -57,23 +57,23 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           LCTL(LGUI(KC_E)),              LCTL(LGUI(KC_U)),KC_6,          KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                         KC_PSCREEN,     KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_PLUS,
-    KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           TG(3),                         KC_DELETE,      KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_ENTER,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                         KC_DELETE,      KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_PLUS,
+    KC_CAPSLOCK,    KC_A,           KC_S,           KC_D,           LT(3,KC_F),     KC_G,           KC_TAB,                        KC_BSPACE,      KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_LSHIFT,      LCTL_T(KC_Z),   KC_X,           KC_C,           LT(4,KC_V),     KC_B,                                                          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),KC_RSHIFT,
-    KC_LCTRL,       WEBUSB_PAIR,    LALT(KC_LSHIFT),KC_LGUI,        LCTL_T(KC_SPACE),KC_F5,                                                        KC_F12,         LT(1,KC_BSPACE),KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,
-                                                                    LSFT_T(KC_TAB), LT(3,KC_ESCAPE), LSFT(LGUI(KC_F)),             LGUI(KC_ENTER), LT(2,KC_ENTER), RSFT_T(KC_ENTER)
+    KC_LCTRL,       WEBUSB_PAIR,    LALT(KC_LSHIFT),LGUI_T(KC_ESC), LCTL_T(KC_SPACE),KC_F5,                                                        KC_F12,         RSFT_T(KC_ENTER),LT(1,KC_LEFT), KC_DOWN,        KC_UP,          KC_RIGHT,
+                                                                    LSFT_T(KC_TAB), TG(3),          LSFT(LGUI(KC_F)),             LGUI(KC_ENTER),  MO(2),          MO(1)
   ),
   [1] = LAYOUT_moonlander(
     KC_BSLASH,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F11,                        KC_F12,         KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_TRANSPARENT,
-    KC_TRANSPARENT, ST_MACRO_Q,     ST_MACRO_W,     ST_MACRO_E,     ST_MACRO_R,     KC_GRAVE,       KC_TRANSPARENT,                ST_MACRO_N,     KC_EXLM,        KC_TILD,        KC_EQUAL,       KC_LBRACKET,    KC_RBRACKET,    KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_AT,          KC_ASTR,        KC_DLR,         KC_PERC,        KC_PIPE,        KC_TRANSPARENT,                ST_MACRO_U,     LSFT(KC_QUOTE), KC_LPRN,        KC_RPRN,        KC_LCBR,        KC_RCBR,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_HASH,        KC_CIRC,        KC_AMPR,                                                       KC_QUOTE,       KC_UNDS,        KC_MINUS,       LSFT(KC_SCOLON),KC_RABK,        KC_DELETE,
+    KC_TRANSPARENT, ST_MACRO_Q,     ST_MACRO_W,     ST_MACRO_E,     ST_MACRO_R,     KC_GRAVE,       KC_TRANSPARENT,                KC_TRANSPARENT, KC_EXLM,        KC_TILD,        KC_EQUAL,       KC_LBRACKET,    KC_RBRACKET,    KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_AT,          KC_ASTR,        KC_DLR,         KC_PERC,        KC_PIPE,        KC_TRANSPARENT,                KC_TRANSPARENT, LSFT(KC_QUOTE), KC_LPRN,        KC_RPRN,        KC_LCBR,        KC_RCBR,        KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_HASH,        KC_CIRC,        KC_AMPR,                                                       KC_QUOTE,       KC_UNDS,        KC_MINUS,       KC_PLUS,        KC_RABK,        KC_DELETE,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                KC_TRANSPARENT, KC_TRANSPARENT, KC_HOME,        KC_PGDOWN,      KC_PGUP,        KC_END,
                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [2] = LAYOUT_moonlander(
     AU_TOG,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                RESET,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_UP,
-    MU_TOG,         KC_TRANSPARENT, KC_MS_UP,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,
+    MU_TOG,         KC_TRANSPARENT, KC_MS_UP,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_HOME,        KC_PGDOWN,      KC_PGUP,        KC_END,         KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,
     MU_MOD,         KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                KC_TRANSPARENT, KC_AUDIO_MUTE,  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RGB_MOD,                                                       KC_TRANSPARENT, KC_MS_BTN1,     KC_MS_BTN2,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -81,15 +81,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [3] = LAYOUT_moonlander(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_SLSH,        KC_ASTR,        KC_MINS,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_PLUS,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_4,           KC_5,           KC_6,           KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                KC_TRANSPARENT, KC_0,           KC_DOT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_F7,          KC_F8,          KC_F9,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_PLUS,        KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_F4,          KC_F5,          KC_F6,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_4,           KC_5,           KC_6,           KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_TRANSPARENT, KC_TRANSPARENT,                                                KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_F10,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                KC_TRANSPARENT, KC_0,           KC_DOT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [4] = LAYOUT_moonlander(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, ST_MACRO_VQ,    ST_MACRO_VW,    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, VIM_QUIT,       VIM_SAVE,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, VIM_COPY,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, VIM_PASTE,      KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -156,9 +156,9 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][4] = {
 
     [3] = {
         {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, // first col
-        {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, // 1
-        {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, // 2
-        {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, // 3
+        {  0,  0,  0}, { 60,255,255}, { 60,255,255}, { 60,255,255}, {  0,  0,  0}, // 1
+        {  0,  0,  0}, { 60,255,255}, { 60,255,255}, { 60,255,255}, {  0,  0,  0}, // 2
+        {  0,  0,  0}, { 60,255,255}, { 60,255,255}, { 60,255,255}, {  0,  0,  0}, // 3
         {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, // 4
         {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0},                // 5
         {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0},                               // -
@@ -254,25 +254,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           break;
 
-      case ST_MACRO_N:
+      case VIM_COPY:
           if (record->event.pressed) {
-              SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_U))) SS_TAP(X_0) SS_TAP(X_0) SS_TAP(X_F) SS_TAP(X_1) SS_TAP(X_ENTER));
+              SEND_STRING(SS_LSFT(SS_TAP(X_QUOTE)) SS_TAP(X_PPLS) SS_TAP(X_Y));
           }
           break;
 
-      case ST_MACRO_U:
+      case VIM_PASTE:
           if (record->event.pressed) {
-              SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_U))) SS_TAP(X_0) SS_TAP(X_0) SS_TAP(X_F) SS_TAP(X_C) SS_TAP(X_ENTER));
+              SEND_STRING(SS_LSFT(SS_TAP(X_QUOTE)) SS_TAP(X_PPLS) SS_TAP(X_P));
           }
           break;
 
-      case ST_MACRO_VQ:
+      case VIM_QUIT:
           if (record->event.pressed) {
               SEND_STRING(SS_TAP(X_ESCAPE) SS_LSFT(SS_TAP(X_SCOLON)) SS_TAP(X_Q) SS_TAP(X_ENTER));
           }
           break;
 
-      case ST_MACRO_VW:
+      case VIM_SAVE:
           if (record->event.pressed) {
               SEND_STRING(SS_TAP(X_ESCAPE) SS_LSFT(SS_TAP(X_SCOLON)) SS_TAP(X_W) SS_TAP(X_ENTER));
           }
